998667d84529c9fa9b2558777c593a5226b6fc42
haywood-d-johnson <haywood.d.johnson@gmail.com>
2024-08-26 17:04:44 -0500

    env: current changes. updated to test

--- src/Components/HeaderComponent/index.js
+++ src/Components/HeaderComponent/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faPuzzlePiece,
@@ -14,46 +14,48 @@ import "./index.css";
 function HeaderComponent() {
     return (
         <header>
-            <nav className="nav_title">
-                <img src="/BlackBoxSoftLogo.png" alt="BBSLogo" />
-            </nav>
-            <nav className="nav-links">
-                <Link to="about" smooth duration={500}>
-                    <FontAwesomeIcon
-                        icon={faPuzzlePiece}
-                        style={{ margin: "5px" }}
-                    />
-                    About
-                </Link>
-                <Link to="skills" smooth duration={500}>
-                    <FontAwesomeIcon
-                        icon={faToolbox}
-                        style={{ margin: "5px" }}
-                    />
-                    Skills
-                </Link>
-                <Link to="projects" smooth duration={500}>
-                    <FontAwesomeIcon
-                        icon={faRocket}
-                        style={{ margin: "5px" }}
-                    />
-                    Projects
-                </Link>
-                <Link to="experience" smooth duration={500}>
-                    <FontAwesomeIcon
-                        icon={faCertificate}
-                        style={{ margin: "5px" }}
-                    />
-                    Experience
-                </Link>
-                <Link to="events" smooth duration={500}>
-                    <FontAwesomeIcon
-                        icon={faCalendarDays}
-                        style={{ margin: "5px" }}
-                    />
-                    Events
-                </Link>
-            </nav>
+            <div className="header-container">
+                <nav className="nav_title">
+                    <img src="/BlackBoxSoftLogo.png" alt="BBSLogo" />
+                </nav>
+                <nav className="nav-links">
+                    <Link to="about" smooth duration={500}>
+                        <FontAwesomeIcon
+                            icon={faPuzzlePiece}
+                            style={{ margin: "5px" }}
+                        />
+                        About
+                    </Link>
+                    <Link to="skills" smooth duration={500}>
+                        <FontAwesomeIcon
+                            icon={faToolbox}
+                            style={{ margin: "5px" }}
+                        />
+                        Skills
+                    </Link>
+                    <Link to="projects" smooth duration={500}>
+                        <FontAwesomeIcon
+                            icon={faRocket}
+                            style={{ margin: "5px" }}
+                        />
+                        Projects
+                    </Link>
+                    <Link to="experience" smooth duration={500}>
+                        <FontAwesomeIcon
+                            icon={faCertificate}
+                            style={{ margin: "5px" }}
+                        />
+                        Experience
+                    </Link>
+                    <Link to="events" smooth duration={500}>
+                        <FontAwesomeIcon
+                            icon={faCalendarDays}
+                            style={{ margin: "5px" }}
+                        />
+                        Events
+                    </Link>
+                </nav>
+            </div>
         </header>
     );
 }
