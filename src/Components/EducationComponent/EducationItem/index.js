9c02f42b1121cd956d27e29bedc1d3959e9bc0a4
Haywood D. Johnson <haywood.d.johnson@gmail.com>
2024-10-22 20:47:46 -0500

    feat: added css for image styling

--- src/Components/EducationComponent/EducationItem/index.js
+++ src/Components/EducationComponent/EducationItem/index.js
@@ -2,12 +2,19 @@ import React from "react";
 
 import "./index.css";
 
-export function ExperienceItem({ type, year, title, details }) {
+export function ExperienceItem({ type, year, title, details, image }) {
+    const hasImage = !!image;
+
     return (
-        <div className="experience-item">
+        <div className={`experience-item ${hasImage ? 'experience-item-with-image' : ''}`}>
             <p className="experience-item-year">{year}</p>
             <h4 className="experience-item-title">{title}</h4>
             <p className="experience-item-details">{details}</p>
+            {hasImage && (
+                <div className="experience-item-image">
+                    <img src={image} alt="Experience item image" />
+                </div>
+            )}
         </div>
     );
 }
