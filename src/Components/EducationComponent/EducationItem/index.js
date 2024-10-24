1cc920c447e021c21f6a9eeded95a8a927da1ab6
haywood-d-johnson <haywood.d.johnson@gmail.com>
2024-10-24 04:32:50 -0500

    feat: added diploma image

--- src/Components/EducationComponent/EducationItem/index.js
+++ src/Components/EducationComponent/EducationItem/index.js
@@ -2,19 +2,26 @@ import React from "react";
 
 import "./index.css";
 
-export function ExperienceItem({ type, year, title, details, image }) {
+export function ExperienceItem({ year, title, details, image }) {
     const hasImage = !!image;
 
     return (
-        <div className={`experience-item ${hasImage ? 'experience-item-with-image' : ''}`}>
-            <p className="experience-item-year">{year}</p>
-            <h4 className="experience-item-title">{title}</h4>
-            <p className="experience-item-details">{details}</p>
+        <div className="experience-item">
             {hasImage && (
                 <div className="experience-item-image">
-                    <img src={image} alt="Experience item image" />
+                    <img
+                        src={image}
+                        alt="Experienceitem"
+                        width="350px"
+                        height="250px"
+                    />
                 </div>
             )}
+            <div className="experience-item-grid">
+                <p className="experience-item-year">{year}</p>
+                <h4 className="experience-item-title">{title}</h4>
+                <p className="experience-item-details">{details}</p>
+            </div>
         </div>
     );
 }
